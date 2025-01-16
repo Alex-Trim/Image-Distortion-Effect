0c92116b05fb9c194475444d99c4f14c1319575a
Alex-Trim <trimaylovalexey@gmail.com>
2025-01-16 11:45:18 +0500

    Добавил изображения

--- src/1.curtain-slider/dumbData.js
+++ src/1.curtain-slider/dumbData.js
@@ -1,43 +1,36 @@
-import img1 from "../assets/img/photo-1542291026-7eec264c27ff.webp";
-import img2 from "../assets/img/photo-1491553895911-0055eca6402d.webp";
-import img3 from "../assets/img/photo-1593081891731-fda0877988da.webp";
-import img4 from "../assets/img/photo-1595950653106-6c9ebd614d3a.webp";
-import img5 from "../assets/img/photo-1597045566677-8cf032ed6634.webp";
-import img6 from "../assets/img/photo-1603631540004-d7b2616b2323.webp";
-
 const unsplashUrls = [
   {
-    url: img1,
+    url: "/img/photo-1542291026-7eec264c27ff.webp",
     title: "Nike Free Flyknit 5.0",
     description:
       "A product microsite to showcase the 1990 ferrari concept designed by the industrial designer George.",
   },
   {
-    url: img2,
+    url: "/img/photo-1491553895911-0055eca6402d.webp",
     title: "Jordan Air 11",
     description:
       "A product microsite to showcase the 1990 ferrari concept designed by the industrial designer George.",
   },
   {
-    url: img3,
+    url: "/img/photo-1593081891731-fda0877988da.webp",
     title: "Running Nike Shoe",
     description:
       "A product microsite to showcase the 1990 ferrari concept designed by the industrial designer George.",
   },
   {
-    url: img4,
+    url: "/img/photo-1595950653106-6c9ebd614d3a.webp",
     title: "Air Jordan 1",
     description:
       "A product microsite to showcase the 1990 ferrari concept designed by the industrial designer George.",
   },
   {
-    url: img5,
+    url: "/img/photo-1597045566677-8cf032ed6634.webp",
     title: "Nike Air Force 1 Shadow",
     description:
       "A product microsite to showcase the 1990 ferrari concept designed by the industrial designer George.",
   },
   {
-    url: img6,
+    url: "/img/photo-1603631540004-d7b2616b2323.webp",
     title: "Ferrari 1990 concept",
     description:
       "A product microsite to showcase the 1990 ferrari concept designed by the industrial designer George.",
